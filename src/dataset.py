f7916564f2284d9340c5175ed3d9460694c11557
dhnhut@gmail.com <dhnhut@gmail.com>
2025-10-01 17:42:47 +1300

    Load datasets

--- src/dataset.py
+++ src/dataset.py
@@ -3,29 +3,32 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader, Dataset
 from PIL import Image
+from torchvision.io import decode_image
 
 
 class SeaTurtleDataset(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.annotations = pd.read_csv(csv_file)
-        self.root_dir = root_dir
+    def __init__(
+        self, annotations_file, img_dir, transform=None, target_transform=None
+    ):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
         self.transform = transform
-
-        # Create a mapping from string labels to integer indices
-        self.label_to_int = {
-            label: i for i, label in enumerate(self.annotations["id"].unique())
-        }
-        self.annotations["id"] = self.annotations["id"].map(self.label_to_int)
+        self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.annotations)
+        return len(self.img_labels)
 
-    def __getitem__(self, index):
-        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
+    def __getitem__(self, idx):
+
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx]["file_name"])
+        # image = decode_image(img_path)
         image = Image.open(img_path).convert("RGB")
-        label = torch.tensor(int(self.annotations.iloc[index, 1]))
+        label = self.img_labels.iloc[idx]["identity"]
 
         if self.transform:
             image = self.transform(image)
+
+        if self.target_transform:
+            label = self.target_transform(label)
 
         return image, label
